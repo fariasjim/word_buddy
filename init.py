100d7fec40ee847e28a1e98c397bb3680831621a
fariasjim <159020530+fariasjim@users.noreply.github.com>
2025-06-20 20:05:05 +0600

    v1.0.1 Published

--- init.py
+++ init.py
@@ -18,50 +18,6 @@ ZIP_URL = f"https://github.com/{REPO_OWNER}/{REPO_NAME}/archive/refs/heads/{BRAN
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMP_EXTRACT_DIR = os.path.join(CURRENT_DIR, "temp_update")
 
-def download_repo_zip():
-    response = requests.get(ZIP_URL)
-    if response.status_code == 200:
-        return response.content
-    else:
-        messagebox.showerror("Error","Update Error")
-
-def extract_zip(content):
-    with zipfile.ZipFile(io.BytesIO(content)) as zip_ref:
-        if os.path.exists(TEMP_EXTRACT_DIR):
-            shutil.rmtree(TEMP_EXTRACT_DIR)
-        zip_ref.extractall(TEMP_EXTRACT_DIR)
-
-def overwrite_current_directory():
-    extracted_subfolder = os.path.join(TEMP_EXTRACT_DIR, f"{REPO_NAME}-{BRANCH}")
-    
-    for item in os.listdir(extracted_subfolder):
-        src = os.path.join(extracted_subfolder, item)
-        dst = os.path.join(CURRENT_DIR, item)
-
-        if os.path.exists(dst):
-            if os.path.isdir(dst):
-                shutil.rmtree(dst)
-            else:
-                os.remove(dst)
-
-        if os.path.isdir(src):
-            shutil.copytree(src, dst)
-        else:
-            shutil.copy2(src, dst)
-
-def restart_script():
-    time.sleep(1)
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-def main():
-    zip_content = download_repo_zip()
-    extract_zip(zip_content)
-    overwrite_current_directory()
-    shutil.rmtree(TEMP_EXTRACT_DIR)
-
-    # Optional: restart
-    restart_script()
-
 url = "https://raw.githubusercontent.com/fariasjim/wordbuddy/refs/heads/main/version.txt"
 response = requests.get(url)
 
@@ -70,12 +26,13 @@ version = "1.0.1\n"  # Current version of application
 print(response.text)
 if response.status_code == 200:
     if response.text == version:
+        Main.main()
         Main.app.mainloop()  # Start the main application loop
     elif response.text == "4000\n":
         messagebox.showerror("Under maintenance", "Update under progress/Author has ended software support")
     else:
         messagebox.showinfo("Update Available","New update available. Press OK to update and restart")
-        main()
+        os.startfile("update.bat")
         
     
 else:
