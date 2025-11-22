4631442d0611deb3ccf235827734f898b2e8470b
fariasjim <159020530+fariasjim@users.noreply.github.com>
2025-11-22 16:43:26 +0600

    ✅Fixed various encoding problems [thanks to Rashed Shan]

--- convertion_logic.py
+++ convertion_logic.py
@@ -9,6 +9,94 @@ runs = []
 def contains_unicode(text):
     # Check if the text contains Unicode characters
     return bool(re.search(r'[^\x00-\x7F]', text))
+unic1 = r"\U+09BC"
+minor_replacement_map = {
+" ‡":" †",
+"K¨y":"Kz¨",
+"MW"+unic1+"‡": "M‡o",
+" ‰":" ˆ",
+"¯—":"¯Í",
+"š—":"šÍ",
+"M­":"Mø",
+"Zª":"Î",
+" ˆ":" ˆ",
+"nÖ":"n«",
+"U¨y":"Uz¨",
+"Ê":"Ð",
+"RvqwK":"RvwqK",
+"¯'vqw":"¯’vwq",
+"KyÐ":"KzÐ",
+"¯c":"¯ú",
+"iª&g":"g©",
+"aÖy":"aªæ",
+"b¨~":"b~¨",
+"¶y":"ÿz",
+"AÖ¨v":"A¨v",
+"Üy":"Üz",
+"¯'~":"¯’‚",
+"l&µ":"®Œ",
+"ù~":"ù‚",
+"gœ":"¤œ",
+"¤ø­":"¤ø",
+"¯ø­":"¯ø",
+"dÖ":"d«",
+"gÖ":"¤ª",
+" ‰":" ˆ",
+"mÖ":"¯ª",
+"±Ö":"±ª",
+"¯Íy":"¯‘",
+"iÖ¨":"i¨",
+"Zowr":"Zwor",
+"å~":"åƒ",
+"™¢~":"™¢‚",
+"¯‹y":"¯‹z",
+"AÖ¨":"A¨",
+"m&K":"¯‹",
+"¯ø­¨":"¯ø¨",
+"K~":"K‚",
+"P~":"P‚",
+"Q~":"Q‚",
+"S~":"S‚",
+"U~":"U‚",
+"V~":"V‚",
+"W~":"W‚",
+"X~":"X‚",
+"Z~":"Z‚",
+"d~":"d‚",
+"eyE":"e~",
+"f~":"f‚",
+"m¨y":"my¨",
+"`¨y":"`yy¨",
+"P¨y":"Pz¨",
+"k¨y":"ï¨",
+"b¨~":"b~¨",
+"Z¨y":"Zz¨",
+"e¨y":"ey¨",
+"RowZ":"RwoZ",
+"O&¶":"•ÿ",
+"Ö":"ª",
+"Ky":"Kz",
+"Py":"Pz",
+"Qy":"Qz",
+"Sy":"Sz",
+"¼y":"¼z",
+"Uy":"Uz",
+"Vy":"Vz",
+"Wy":"Wz",
+"O&¸":"½y",
+"Xy":"Xz",
+"Zy":"Zz",
+"dy":"dz",
+"fy":"fz",
+"oy":"o–",
+"‡o":"o‡",
+"qww":"wqw",
+"cowqv":"cwoqv"
+}
+def minor_fixes(match):
+    matched_char = match.group(0)
+    return minor_replacement_map.get(matched_char, matched_char)
+pattern = r"|".join(re.escape(key) for key in minor_replacement_map.keys())
 
 def replace_and_highlight(doc_path, save_path, h_value):
     doc = Document(doc_path)
@@ -18,17 +106,26 @@ def replace_and_highlight(doc_path, save_path, h_value):
     
     for paragraph in doc.paragraphs:
         for run in paragraph.runs:
+            print(run)
             if contains_unicode(run.text):
                 runs.append(run)
                 if contains_unicode(run.text) and run.font.name == "SolaimanLipi":
             # Convert the text from Unicode to Bijoy
                     converted_text = unicode_converter.convertUnicodeToBijoy(run.text)
+                    converted_text = re.sub(pattern, minor_fixes, converted_text)
+                    if converted_text[0] == '‡':
+                        # Create a new string: '†' + everything from the 2nd character onward
+                        converted_text = '†' + converted_text[1:]
+                    if converted_text[0] == '‰':
+                        # Create a new string: '†' + everything from the 2nd character onward
+                        converted_text = 'ˆ' + converted_text[1:]
                     if run.text != converted_text:
                         run.text = converted_text
                         if h_value == 1:
                             run.font.highlight_color = WD_COLOR_INDEX.TURQUOISE  # Teal highlight
                         run.font.name = "SutonnyMJ"  # Set font to Bijoy
                         runs.append(run)
+                        print(run.text)
                         
 
     
